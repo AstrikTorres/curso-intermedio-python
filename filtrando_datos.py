601aa341b41a0080e303553104a3f306add50e67
Astrik Torres <astriktorres@gmail.com>
2021-11-22 16:23:02 -0600

    Proyecto: Filtrando datos

---
+++ filtrando_datos.py
@@ -0,0 +1,105 @@
+DATA = [
+    {
+        'name': 'Facundo',
+        'age': 72,
+        'organization': 'Platzi',
+        'position': 'Technical Coach',
+        'language': 'python',
+    },
+    {
+        'name': 'Luisana',
+        'age': 33,
+        'organization': 'Globant',
+        'position': 'UX Designer',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Héctor',
+        'age': 19,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'ruby',
+    },
+    {
+        'name': 'Gabriel',
+        'age': 20,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Isabella',
+        'age': 30,
+        'organization': 'Platzi',
+        'position': 'QA Manager',
+        'language': 'java',
+    },
+    {
+        'name': 'Karo',
+        'age': 23,
+        'organization': 'Everis',
+        'position': 'Backend Developer',
+        'language': 'python',
+    },
+    {
+        'name': 'Ariel',
+        'age': 32,
+        'organization': 'Rappi',
+        'position': 'Support',
+        'language': '',
+    },
+    {
+        'name': 'Juan',
+        'age': 17,
+        'organization': '',
+        'position': 'Student',
+        'language': 'go',
+    },
+    {
+        'name': 'Pablo',
+        'age': 32,
+        'organization': 'Master',
+        'position': 'Human Resources Manager',
+        'language': 'python',
+    },
+    {
+        'name': 'Lorena',
+        'age': 56,
+        'organization': 'Python Organization',
+        'position': 'Language Maker',
+        'language': 'python',
+    },
+]
+
+
+def run():
+    # Con list comprehensions
+    all_python_devs = [worker["name"] for worker in DATA if worker["language"] == "python"]
+
+    all_Platzi_workers = [worker["name"] for worker in DATA if worker["organization"] == "Platzi"]
+
+    # Reto:
+
+    adults_reto = [worker['name'] for worker in DATA if worker['age'] > 18]
+
+    old_people_reto = [{**worker, **{'old': worker['age'] > 70}} for worker in DATA]
+
+    # Con high order functions
+
+    adults = list(filter(lambda worker: worker["age"] > 18, DATA))
+    adults = list(map(lambda worker: worker["name"], adults))
+
+    old_people = list(map(lambda worker: worker | {"old": worker["age"] > 70}, DATA))
+
+    # Reto:
+    all_python_devs_reto = list(filter(lambda worker: worker['language'] == 'python', DATA))
+    all_python_devs_reto = list(map(lambda worker: worker['name'], all_python_devs_reto))
+
+    all_platzi_workers_reto = list(filter(lambda worker: worker['organization'] == 'Platzi', DATA))
+    all_platzi_workers_reto = list(map(lambda worker: worker['name'], all_platzi_workers_reto))
+    for worker in all_platzi_workers_reto:
+        print(worker)
+
+
+if __name__ == '__main__':
+    run()
